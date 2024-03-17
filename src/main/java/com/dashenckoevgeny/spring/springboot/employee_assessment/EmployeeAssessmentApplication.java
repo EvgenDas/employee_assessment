f229fef99e6008da39f2929099c1de01fbd19423
Dashencko Evgeny <j.dashencko@gmail.com>
2024-03-17 19:00:30 +0300

    fix ingress and auth

--- src/main/java/com/dashenckoevgeny/spring/springboot/employee_assessment/EmployeeAssessmentApplication.java
+++ src/main/java/com/dashenckoevgeny/spring/springboot/employee_assessment/EmployeeAssessmentApplication.java
@@ -1,5 +1,7 @@
 package com.dashenckoevgeny.spring.springboot.employee_assessment;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -8,6 +10,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableCaching
+@OpenAPIDefinition(servers = { @Server(url = "http://localhost:8888")})
 public class EmployeeAssessmentApplication {
 
 	public static void main(String[] args) {
