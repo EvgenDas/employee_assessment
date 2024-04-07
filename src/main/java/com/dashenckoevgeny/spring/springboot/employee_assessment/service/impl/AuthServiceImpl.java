991c83a59b0da0d3dafc6bf2cab66f14ba1b0b1c
Dashencko Evgeny <j.dashencko@gmail.com>
2024-04-07 19:34:02 +0300

    fix

--- src/main/java/com/dashenckoevgeny/spring/springboot/employee_assessment/service/impl/AuthServiceImpl.java
+++ src/main/java/com/dashenckoevgeny/spring/springboot/employee_assessment/service/impl/AuthServiceImpl.java
@@ -9,6 +9,7 @@ import com.dashenckoevgeny.spring.springboot.employee_assessment.web.security.Jw
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
 
@@ -21,23 +22,24 @@ public class AuthServiceImpl implements AuthService {
   private final JwtTokenProvider jwtTokenProvider;
 
   @Override
-  public JwtResponse login(final JwtRequest loginRequest){
+  public JwtResponse login(final JwtRequest loginRequest) {
     JwtResponse jwtResponse = new JwtResponse();
-    try {
-      authenticationManager.authenticate(
-          new UsernamePasswordAuthenticationToken(loginRequest.getLogin(),
-              loginRequest.getPassword()));
-    } catch (AuthenticationException e) {
 
+    Authentication authentication = authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(loginRequest.getLogin(),
+            loginRequest.getPassword()));
+
+    if (authentication.isAuthenticated()) {
+      Employee employee = employeeService.getByLogin(loginRequest.getLogin());
+      jwtResponse.setId(employee.getId());
+      jwtResponse.setUsername(employee.getLogin());
+      jwtResponse.setAccessToken(jwtTokenProvider.createAccessToken(employee.getId(),
+          employee.getLogin(), employee.getRoles()));
+      jwtResponse.setRefreshToken(jwtTokenProvider.createRefreshToken(employee.getId(),
+          employee.getLogin()));
+      return jwtResponse;
     }
-    Employee employee = employeeService.getByLogin(loginRequest.getLogin());
-    jwtResponse.setId(employee.getId());
-    jwtResponse.setUsername(employee.getLogin());
-    jwtResponse.setAccessToken(jwtTokenProvider.createAccessToken(employee.getId(),
-        employee.getLogin(), employee.getRoles()));
-    jwtResponse.setRefreshToken(jwtTokenProvider.createRefreshToken(employee.getId(),
-        employee.getLogin()));
-    return jwtResponse;
+    return null;
   }
 
   @Override
